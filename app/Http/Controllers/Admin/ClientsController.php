5c9a93705c27d1231a00fe6b97e06af0004f6338
Miroslav Grofcik <miroslav.grofcik.m@gmail.com>
2019-09-17 12:51:14 +0100

    Add controller (admin-clients-1)

---
+++ app/Http/Controllers/Admin/ClientsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Client;
+use Illuminate\Http\Request;
+
+class ClientsController extends AdminController
+{
+    public function index (){
+        $clients = Client::orderBy('created_at', 'desc')->get();
+
+        return view('admin.clients.index', compact('clients'));
+    }
+
+    public function create(){
+        return view('admin.clients.create');
+    }
+
+    public function store(Request $request){
+        $client = Client::create($request->all());
+
+        $client->save();
+
+        //$this->upload_image($request, 'image', 'clients', $client, 'profile');
+
+        $this->_setFlashMessage($request,'success', "Klient <b>$client->name_sk</b> úspešne vytvorený");
+
+        return redirect()->route('clients.index');
+    }
+
+    public function edit($id){
+        $client = Client::findOrFail($id);
+
+        return view('admin.clients.edit', compact('client'));
+    }
+
+    public function update(Request $request, $id){
+        $client = Client::findOrFail($id);
+
+        $client->update($request->all());
+
+        $client->save();
+
+        //$this->upload_image($request,'image', 'clients', $client, 'profile');
+
+        $this->_setFlashMessage($request, 'success', "Klient <b>$client->name_sk</b> úspešne zmenený");
+
+        return back();
+    }
+
+    public function delete(Request $request, $id){
+        $client = Client::findOrFail($id);
+
+        $this->_setFlashMessage($request, 'success', "Klient <b>$client->name_sk</b> úspešne vymazaný" );
+
+        /*
+        if(count($client->images) > 0){
+            foreach($client->images as $image){
+                $image->delete();
+            }
+        }*/
+
+        $client->delete();
+
+        return redirect()->route('clients.index');
+    }
+}
